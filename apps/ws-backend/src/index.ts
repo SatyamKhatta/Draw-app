baf44cb6ee2696160eb2bffd2e677e3eb1c06a9b
SatyamKhatta <149493210+SatyamKhatta@users.noreply.github.com>
2025-04-04 23:05:22 +0530

    added ws logic

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,11 +1,32 @@
-import {WebSocketServer} from "ws";
+// import {WebSocketServer} from "ws";
+import { WebSocket, WebSocketServer } from "ws";  // Import WebSocket from ws
 import jwt, { JwtPayload } from "jsonwebtoken"
 // import { JWT_SECRET } from "@repo/backend-common/config";
 const { JWT_SECRET } = require("@repo/backend-common/config");
 
 const wss = new WebSocketServer({port:8080})
-
 console.log("WebSocket server started on port 8080");
+
+interface User{
+  ws:WebSocket,
+  rooms:string[],
+  userId:string
+}
+const users:User[]=[];
+
+function checkUser(token:string):string | null{
+  
+  const decoded = jwt.verify(token,JWT_SECRET);
+
+  if(typeof decoded == "string" ){
+   
+    return null;
+  }
+  if(!decoded || !decoded.userId){
+    return null;
+  }
+  return decoded.userId;
+}
 
 wss.on("connection",function connection(ws,request) {
 
@@ -17,17 +38,20 @@ if(!url){
   const queryParams= new URLSearchParams(url.split('?')[1])
   const token = queryParams.get('token') || "";
 
-  const decoded = jwt.verify(token,JWT_SECRET);
+  const userId=checkUser(token);
 
-  if(typeof decoded == "string" ){
+
+  if(userId == null) {
     ws.close();
-    return;
-  }
-  if(!decoded || !(decoded as JwtPayload).userId){
-    ws.close();
-    return; 
+    return null;
   }
 
+  users.push({
+    userId,
+    rooms: [],
+    ws
+  })
+ 
     ws.on("message",function message(data) {
         ws.send("pong");
     })
